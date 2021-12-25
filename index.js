77d7be54312216d32c3ebf65f9f60e92254bf101
Tarun Ravi <tarunvravi@gmail.com>
2021-12-24 19:35:26 -0500

    d

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const cors = require("cors");
 
 app.use(cors());
 
+app.use(express.static("build"));
+
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
   console.log("Path:  ", request.path);
@@ -37,10 +39,6 @@ let notes = [
     important: true,
   },
 ];
-
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
-});
 
 app.get("/api/notes", (req, res) => {
   res.json(notes);
